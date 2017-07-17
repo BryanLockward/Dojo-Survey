d2799a95d9b0d28db72a7d4dffe0d641ae46cbb8
Bryan Lockward <bmlockward3@verizon.net>
2017-07-17 10:15:14 -0400

    Validation Added

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request , redirect
+from flask import Flask, render_template, request , redirect, flash
 app = Flask(__name__)
+app.secret_key="key"
 
 @app.route('/')
 def survey():
@@ -7,10 +8,31 @@ def survey():
 
 @app.route('/results',methods=['POST'])
 def post_results():
+
     name = request.form['name']
+    if len(name)<1:
+        flash("Error, NAME cannot be blank!")
+        return redirect("/")
+
     email = request.form['email']
+    if len(email)<1:
+        flash("Error, Email cannot be blank!")
+        return redirect("/")
+
     location = request.form['location']
+    if len(location)<1:
+        flash("Error, Location cannot be blank!")
+        return redirect("/")
+
     comments = request.form['comments']
+    if len(comments)>120:
+        flash("Error, Comments cannot be more than 120 characters!")
+        return redirect("/")
+
+    elif len(comments)<1:
+        flash("Error, Comments cannot be blank!")
+        return redirect("/")
+
     return render_template("results.html",name=name,email=email,location=location,comments=comments)
 
 
